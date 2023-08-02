205ee5069141882dc0b01a1781b210c577d80f82
alishakor <aliuokeowo@gmail.com>
2023-08-02 15:31:10 +0100

    correcting the value of an array

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,7 @@
 export default function getListStudents() {
   const studentArray = [
     {
-      id: 1, firstName: 'Guillame', location: 'San Francisco',
+      id: 1, firstName: 'Guillaume', location: 'San Francisco',
     },
     {
       id: 2, firstName: 'James', location: 'Columbia',

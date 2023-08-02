9a9cb4cfb326b0587d8c31177ccb7171807f59be
alishakor <aliuokeowo@gmail.com>
2023-08-02 11:39:57 +0100

    task 0, README and config files for data manipulation

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,15 @@
+export default function getListStudents() {
+  const studentArray = [
+    {
+      id: 1, firstName: 'Guillame', location: 'San Francisco'
+    },
+    {
+      id: 2, firstName: 'James', location: 'Columbia'
+    },
+    {
+      id: 5, firstName: 'Serena', location: 'San Francisco'
+    }
+  ];
+
+  return studentArray;
+}
